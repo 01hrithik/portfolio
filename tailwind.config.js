c86b10e484c7c2f04950a5250a0b8669a23aba55
Hrithik Gorane <hrithikgorane@gmail.com>
2024-09-01 15:14:45 +0530

    created an Portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: { transitionProperty: {
+      'height': 'height',
+      'spacing': 'margin, padding',
+    }},
+  },
+  plugins: [],
+}
+
